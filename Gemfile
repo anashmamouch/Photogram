ff94976e0f34c66872c1177d6b1da1aeddf8d4a8
Anas Hmamouch <anashmamouch90@gmail.com>
2015-08-10 20:10:26 +0100

    showing all posts in the index page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem "paperclip", "~> 4.3"
+gem 'simple_form', '~> 3.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #creating the post and saving it into the database
+    @post = Post.create(post_params)
+    #redirect back to the index page
+    redirect_to posts_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:image, :caption)
+    end
 end
