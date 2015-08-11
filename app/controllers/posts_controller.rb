48cde8fc9ba737c9e58904bece6a552dbf095b7f
Anas Hmamouch <anashmamouch90@gmail.com>
2015-08-11 15:15:40 +0100

    Finished the first part... :tada:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @posts = Post.all
   end
@@ -10,12 +13,45 @@ class PostsController < ApplicationController
 
   def create
     #creating the post and saving it into the database
-    @post = Post.create(post_params)
-    #redirect back to the index page
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      #redirect back to the index page
+      flash[:success] = "Your post has been created."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update post_params
+      flash[:success] = "Post updated."
+      redirect_to post_params
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
     def post_params
       params.require(:post).permit(:image, :caption)
     end
